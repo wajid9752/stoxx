f421be9d124c80a4f28c3bb5721bec72c4599028
wazid Ali <developer9752@gmail.com>
2021-10-06 09:44:45 +0530

    frontend file added

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: waitress-serve --port=$PORT tayyab.wsgi:application
--- tayyab/settings.py
+++ tayyab/settings.py
@@ -151,7 +151,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT=BASE_DIR / 'static'
 # STATICFILES_DIRS = [
-    
 #     os.path.join(BASE_DIR, 'static')
 # ]
 MEDIA_URL = '/media/'
